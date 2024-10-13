d6213a378247b2bac5350de2465368c988025408
Anna Kroka <avkroka.broker@gmail.com>
2024-10-13 02:14:00 -0700

    add menu

--- src/js/header.js
+++ src/js/header.js
@@ -1,90 +1,3 @@
-// (() => {
-//     // Находим все элементы меню
-//     const mobileMenus = document.querySelectorAll('.js-menu-container');
-//     const openMenuBtn = document.querySelector('.js-open-menu');
-//     const closeMenuBtn = document.querySelector('.js-close-menu');
-  
-//     // Проверяем наличие необходимых элементов
-//     if (!openMenuBtn || !closeMenuBtn || mobileMenus.length === 0) return;
-  
-//     // Функция для переключения состояния меню
-//     const toggleMenu = () => {
-//       const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true';
-  
-//       // Обновляем атрибут для управления состоянием меню
-//       openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-  
-//       // Переключаем класс 'is-open' для всех меню
-//       mobileMenus.forEach(menu => {
-//         menu.classList.toggle('is-open', !isMenuOpen);
-  
-//         // Обработчики событий для якорных ссылок
-//         const anchors = menu.querySelectorAll('a[href*="#"]');
-//         anchors.forEach(anchor => {
-//           if (!isMenuOpen) {
-//             anchor.addEventListener("click", closeMenuOnAnchorClick);
-//           } else {
-//             anchor.removeEventListener("click", closeMenuOnAnchorClick);
-//           }
-//         });
-//       });
-  
-//       // Переключаем блокировку прокрутки тела страницы
-//       const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
-//       if (typeof bodyScrollLock !== 'undefined' && typeof bodyScrollLock[scrollLockMethod] === 'function') {
-//         bodyScrollLock[scrollLockMethod](document.body);
-//       }
-//     };
-  
-//     // Функция для закрытия меню при клике на якорную ссылку
-//     const closeMenuOnAnchorClick = () => {
-//       openMenuBtn.setAttribute('aria-expanded', false);
-//       mobileMenus.forEach(menu => {
-//         menu.classList.remove('is-open');
-//       });
-//       if (typeof bodyScrollLock !== 'undefined' && typeof bodyScrollLock.enableBodyScroll === 'function') {
-//         bodyScrollLock.enableBodyScroll(document.body);
-//       }
-//     };
-  
-//     // Добавляем обработчики событий для открытия и закрытия меню
-//     openMenuBtn.addEventListener('click', toggleMenu);
-//     closeMenuBtn.addEventListener('click', toggleMenu);
-  
-//     // Закрываем меню при изменении ширины окна до указанного брейкпоинта
-//     window.matchMedia('(min-width: 375px)').addEventListener('change', e => {
-//       if (!e.matches) return;
-  
-//       mobileMenus.forEach(menu => {
-//         menu.classList.remove('is-open');
-//       });
-  
-//       openMenuBtn.setAttribute('aria-expanded', false);
-//       if (typeof bodyScrollLock !== 'undefined' && typeof bodyScrollLock.enableBodyScroll === 'function') {
-//         bodyScrollLock.enableBodyScroll(document.body);
-//       }
-//     });
-  
-//     document.addEventListener('DOMContentLoaded', () => {
-//       // Найти все элементы с атрибутом data-menu-close
-//       const closeButtons = document.querySelectorAll('[data-menu-close]');
-  
-//       // Добавить обработчик события клика для каждого элемента
-//       closeButtons.forEach(button => {
-//         button.addEventListener('click', () => {
-//           mobileMenus.forEach(menu => {
-//             menu.classList.remove('is-open');
-//           });
-//           openMenuBtn.setAttribute('aria-expanded', false);
-//           if (typeof bodyScrollLock !== 'undefined' && typeof bodyScrollLock.enableBodyScroll === 'function') {
-//             bodyScrollLock.enableBodyScroll(document.body);
-//           }
-//         });
-//       });
-//     });
-//   })();
-
-
 //   Dark mode
 
 const chk = document.getElementById("chk");
@@ -95,8 +8,8 @@ chk.addEventListener("change", () => {
 
 // Mobile menu
 
-const menu = document.querySelector(".nav-list-header");
-const menuItems = document.querySelectorAll(".nav-link-header");
+const menu = document.querySelector(".menu-header");
+const menuItems = document.querySelectorAll(".menuItem-header");
 const hamburger= document.querySelector(".btn-menu");
 const closeIcon= document.querySelector(".btn-close-icon");
 const menuIcon = document.querySelector(".btn-menu-icon");
