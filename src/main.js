f7ea0ade290ff10f8a679a411fe1f0907c79673d
Galushko Evgeniy <yudginchik@gmail.com>
2024-10-13 13:17:01 +0300

    add

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // import Swiper JS
-import Swiper from 'swiper';
+// import Swiper from 'swiper';
 // import Swiper styles
-import 'swiper/css';
+// import 'swiper/css';
 
 import './js/work-together';
