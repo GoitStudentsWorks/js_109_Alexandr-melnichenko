1c175ef9bcd3058d092356bef00953a74e3fa646
mariia <william.starlung@gmail.com>
2024-10-15 13:36:16 -0400

    finishing touches with background and color

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,8 +1,5 @@
 import Accordion from 'accordion-js';
 // import 'accordion-js/dist/accordion.min.css';
-// import iconUp from "../img/icons.svg"
-const dowm = '<svg class="faq-icon" width="16" height="9" ><use href="../img/faq-icons.svg#icon-faq-down"></svg>'
-const up = '<svg class="faq-icon" width="16" height="9"><use href="../img/faq-icons.svg#icon-faq-up"></svg>'
 
 new Accordion('.accordion-container', {
     openOnInit: [0],
